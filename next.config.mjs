ede7fb758a909489303cb9b313add7d8df3eb302
Anh Truong Nguyen <truongnguyen4042@gmail.com>
2024-01-23 00:55:38 +1000

    add token

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    /** @type {import('next').NextConfig} */
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'my-blob-store.public.blob.vercel-storage.com',
+                port: '',
+            },
+        ],
+    },
+};
 
 export default nextConfig;
