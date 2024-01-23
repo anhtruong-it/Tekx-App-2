c2870258c550d1cb5dee8c44f8556f940977512f
Anh Truong Nguyen <truongnguyen4042@gmail.com>
2024-01-23 12:33:11 +1000

    uploading effect

--- src/app/image/uploadForm.js
+++ src/app/image/uploadForm.js
@@ -1,41 +1,57 @@
 'use client';
+import LoadingButton from '@/components/loadingButton';
 import { useState, useRef } from 'react';
 
 export default function UploadForm() {
   const inputFileRef = useRef(null);
   const [blob, setBlob] = useState(null);
+  const [uploading, setUploading] = useState(false);
+
+  const handleUpload = async (event) => {
+    event.preventDefault();
+
+    const file = inputFileRef.current.files[0];
+
+    try {
+      // Set uploading state to true
+      setUploading(true);
+
+      const response = await fetch(`/api/image?filename=${file.name}`, {
+        method: 'POST',
+        body: file,
+      });
+
+      const newBlob = await response.json();
+      console.log(newBlob);
+
+      // Set the new blob in the state
+      setBlob(newBlob);
+    } finally {
+      // Reset uploading state to false
+      setUploading(false);
+      
+    }
+  };
 
   return (
     <>
       <h1>Upload Your Avatar</h1>
 
-      <form
-        onSubmit={async (event) => {
-          event.preventDefault();
+      {uploading ? (
+        <LoadingButton variant="blue" text="Uploading..." />
+      ) : (
+        <form onSubmit={handleUpload}>
+          <input name="file" ref={inputFileRef} type="file" required />
+          <button type="submit">Upload</button>
+        </form>
+      )}
 
-          const file = inputFileRef.current.files[0];
-
-          const response = await fetch(`/api/image?filename=${file.name}`, {
-            method: 'POST',
-            body: file,
-          });
-
-          const newBlob = await response.json();
-          console.log(newBlob);
-
-          setBlob(newBlob);
-        }}
-      >
-        <input name="file" ref={inputFileRef} type="file" required />
-        <button type="submit">Upload</button>
-      </form>
-
-      {blob && (
+      {/* {blob && (
         // Test URL uploaded image
         <div>
           Blob url: <a href={blob.url}>{blob.url}</a>
         </div>
-      )}
+      )} */}
     </>
   );
 }
